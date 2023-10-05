574248d7f30c7f38e336ec3d5460f4ec95d0cdad
Edgar Concepcion <econc017@fiu.edu>
2023-10-04 20:52:39 -0400

    Uploaded Finished Group Project

---
+++ GroupProject/PicoBillBoard.py
@@ -0,0 +1,209 @@
+"""
+A basic template file for using the Model class in PicoLibrary
+This will allow you to implement simple Statemodels with some basic
+event-based transitions.
+
+Currently supports only 4 buttons (hardcoded to BTN1 through BTN4)
+and a TIMEOUT event for internal tranisitions.
+
+For processing your own events such as sensors, you can implement
+those in your run method for transitions based on sensor events.
+"""
+
+# Import whatever Library classes you need - Model is obviously needed
+import time
+import random
+from Model import *
+from Button import *
+from Counters import *
+from Lights import *
+from CompositeLights import *
+from Displays import *
+import json
+
+"""
+This is the template Model Runner - you should rename this class to something
+that is supported by your class diagram. This should associate with your other
+classes, and any PicoLibrary classes. If you are using Buttons, you will implement
+buttonPressed and buttonReleased.
+
+To implement the model, you will need to implement 3 methods to support entry actions,
+exit actions, and state actions.
+
+This template currently implements a very simple state model that uses a button to
+transition from state 0 to state 1 then a 5 second timer to go back to state 0.
+"""
+
+class PicoBillBoard:
+
+    def __init__(self):
+        
+        # Instantiate whatever classes from your own model that you need to control
+        # Handlers can now be set to None - we will add them to the model and it will
+        # do the handling
+        self._nextB = Button(10, "button1", buttonhandler=None)
+        self._previousB = Button(12, "button3", buttonhandler=None)
+        self._partyB = Button(11, "button2", buttonhandler=None)
+        self._timer = SoftwareTimer(None)
+        self._partylight = NeoPixel(pin=17, numleds=8, brightness=1)
+        self._display = LCDDisplay(sda=0, scl=1, i2cid=0)
+        self._adList = {} # Jsons are Dictionaries
+        self._index = -1
+        
+        # Instantiate a Model. Needs to have the number of states, self as the handler
+        # You can also say debug=True to see some of the transitions on the screen
+        # Here is a sample for a model with 4 states
+        self._model = Model(5, self, debug=True)
+        
+        # Up to 4 buttons and a timer can be added to the model for use in transitions
+        # Buttons must be added in the sequence you want them used. The first button
+        # added will respond to BTN1_PRESS and BTN1_RELEASE, for example
+        self._model.addButton(self._nextB)
+        self._model.addButton(self._partyB)
+        self._model.addButton(self._previousB)
+        # add other buttons (up to 3 more) if needed
+        
+        # Add any timer you have.
+        self._model.addTimer(self._timer)
+        
+        # Now add all the transitions that are supported by my Model
+        # obvously you only have BTN1_PRESS through BTN4_PRESS
+        # BTN1_RELEASE through BTN4_RELEASE
+        # and TIMEOUT
+        
+        # some examples:
+        self._model.addTransition(0, [BTN1_PRESS], 1)
+        self._model.addTransition(2, [TIMEOUT], 0)
+        self._model.addTransition(3, [TIMEOUT], 3)
+        self._model.addTransition(3, [BTN2_PRESS], 4)
+        self._model.addTransition(4, [BTN2_PRESS], 3)
+        # etc.
+    
+    """
+    Create a run() method - you can call it anything you want really, but
+    this is what you will need to call from main.py or someplace to start
+    the state model.
+    """
+
+    def run(self):
+        # The run method should simply do any initializations (if needed)
+        # and then call the model's run method.
+        # You can send a delay as a parameter if you want something other
+        # than the default 0.1s. e.g.,  self._model.run(0.25)
+        self._model.run()
+
+    def pullServerList(self):
+        try:
+            ads = open('sample_ad_list.json', 'r') # wont find this file
+            self._adList = json.load(ads)
+            ads.close()
+        except OSError: # Error catch
+            ads = open('sample_ad_list1.json', 'r') # will find this file
+            self._adList = json.load(ads)
+            ads.close()
+            self._model.gotoState(3)
+        except: # Go to state 2 to let the user know there was an error, and try again
+            self._model.gotoState(2)
+
+    """
+    stateDo - the method that handles the do/actions for each state
+    """
+    def stateDo(self, state):
+            
+        # Now if you want to do different things for each state you can do it:
+        if state == 0:
+            # State 0 do/actions
+            pass
+        elif state == 1:
+            # State1 do/actions
+            # You can check your sensors here and perform transitions manually if needed
+            # For example, if you want to go from state 1 to state 2 when the motion sensor
+            # is tripped you can do something like this
+            # if self.motionsensor.tripped():
+            # 	gotoState(2)
+            pass
+        elif state == 2:
+            self._timer.check()
+        elif state == 3:
+            self._timer.check()
+            self._display.showText(str(self._adList["Ad_List"][self._index]["Ad_Text"]))
+            if self._nextB.isPressed(): 
+                self._model.gotoState(3)
+            elif self._previousB.isPressed(): 
+                self._index = self._index-2
+                self._model.gotoState(3)
+        elif state == 4:
+            pass
+            
+
+    """
+    stateEntered - is the handler for performing entry/actions
+    You get the state number of the state that just entered
+    Make sure actions here are quick
+    """
+    def stateEntered(self, state, event):
+        # Again if statements to do whatever entry/actions you need
+        if state == 0:
+            # entry actions for state 0
+            print('State 0 entered')
+            self._index=-1
+            self._display.showText(f"                               ")
+            self._display.showText(f"YOUR AD HERE    PICO ADS ")
+        elif state == 1:
+            # entry actions for state 1
+            print('State 1 entered')
+            # Load the ads
+            self.pullServerList()
+            print(self._adList) # Print adList to test
+        elif state == 2:
+            # Entry actions for state 2
+            print('State 2 entered')
+            self._display.showText(f"                               ")
+            self._display.showText(f"Error pulling adlist           ")
+            self._timer.start(5)
+        elif state == 3:
+            # Entry actions for state 2
+            print('State 3 entered')
+            self._display.reset()
+            self._index +=1
+            if self._index > len(self._adList):
+                self._model.gotoState(0)
+            self._timer.start(self._adList["Ad_List"][self._index]["Time_s"])
+        elif state == 4:
+            # Entry actions for state 2
+            print('State 4 entered')
+            self._partylight.run(NeoPixel.RAINBOW)
+        
+            
+    """
+    stateLeft - is the handler for performing exit/actions
+    You get the state number of the state that just entered
+    Make sure actions here are quick
+    
+    This is just like stateEntered, perform only exit/actions here
+    """
+
+    def stateLeft(self, state, event):
+        if state == 1:
+            # Exit actions for state 1
+            print('State 1 Left')
+            pass
+        elif state == 2:
+            # Entry actions for state 2
+            print('State 2 Left')
+            self._timer.cancel()
+        elif state == 3:
+            # Entry actions for state 2
+            print('State 3 Left')
+            self._timer.cancel()
+        elif state == 4:
+            # Entry actions for state 2
+            print('State 4 Left')
+            self._partylight.off()
+            
+
+# Test your model. Note that this only runs the template class above
+# If you are using a separate main.py or other control script,
+# you will run your model from there.
+if __name__ == '__main__':
+    MyControllerTemplate().run()
